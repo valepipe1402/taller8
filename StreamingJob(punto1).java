9378245c88a49a2ed27d35e4a3143b44d47c9b62
valepipe1402 <40077221+valepipe1402@users.noreply.github.com>
2018-06-09 14:10:49 -0500

    Add files via upload

---
+++ StreamingJob(punto1).java
@@ -0,0 +1,83 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.flink.quickstart;
+
+import com.dataartisans.flinktraining.exercises.datastream_java.datatypes.TaxiFare;
+import com.dataartisans.flinktraining.exercises.datastream_java.datatypes.TaxiRide;
+import com.dataartisans.flinktraining.exercises.datastream_java.sources.TaxiFareSource;
+import com.dataartisans.flinktraining.exercises.datastream_java.sources.TaxiRideSource;
+import com.dataartisans.flinktraining.exercises.datastream_java.utils.GeoUtils;
+import org.apache.flink.api.common.functions.FilterFunction;
+import org.apache.flink.api.java.utils.ParameterTool;
+import org.apache.flink.streaming.api.TimeCharacteristic;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+/**
+ * Skeleton for a Flink Streaming Job.
+ *
+ * <p>For a tutorial how to write a Flink streaming application, check the
+ * tutorials and examples on the <a href="http://flink.apache.org/docs/stable/">Flink Website</a>.
+ *
+ * <p>To package your appliation into a JAR file for execution, run
+ * 'mvn clean package' on the command line.
+ *
+ * <p>If you change the name of the main class (with the public static void main(String[] args))
+ * method, change the respective entry in the POM.xml file (simply search for 'mainClass').
+ */
+public class StreamingJob {
+
+	public static void main(String[] args) throws Exception {
+
+		//ParameterTool params = ParameterTool.fromArgs(args);
+		//final String input = params.getRequired("/home/bigdata/nycTaxiRides.gz");
+
+		final int maxEventDelay = 60;       // events are out of order by max 60 seconds
+		final int servingSpeedFactor = 600; // events of 10 minutes are served in 1 second
+
+		// set up streaming execution environment
+		StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+		env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
+
+		// start the data generator
+		DataStream<TaxiRide> rides = env.addSource(
+				new TaxiRideSource("/home/bigdata/nycTaxiRides.gz", maxEventDelay, servingSpeedFactor));
+
+		DataStream<TaxiRide> filteredRides = rides
+				// filter out rides that do not start or stop in NYC
+				.filter(new NYCFilter());
+
+		// print the filtered stream
+		filteredRides.print();
+
+		// run the cleansing pipeline
+		env.execute("Taxi Ride Cleansing");
+	}
+
+
+	public static class NYCFilter implements FilterFunction<TaxiRide> {
+
+		@Override
+		public boolean filter(TaxiRide taxiRide) throws Exception {
+
+			return GeoUtils.isInNYC(taxiRide.startLon, taxiRide.startLat) &&
+					GeoUtils.isInNYC(taxiRide.endLon, taxiRide.endLat);
+		}
+	}
+}
